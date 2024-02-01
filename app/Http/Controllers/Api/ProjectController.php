a0f6f0a4b794913386be246d7a60943a59f7f572
MarcoGranata9 <granatam9900@gmail.com>
2024-02-01 15:52:08 +0100

    add: type on projects api query

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
+use App\Models\Technology;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
     public function index() {
-        $projects = Project::all();
+        $projects = Project::with('type')->get();
         return response()->json([
             'results' => $projects,
             'success' => true,
